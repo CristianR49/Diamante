7fb21ef3c1abf42be01702734225dbcc12395e38
Cristian <cristian.rr99@gmail.com>
2023-03-16 13:51:12 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Diamante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("De um número N ímpar, será desenhado um diamante de x com N altura e N largura");
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine("Número par, digite novamente");
+                    continue;
+                }
+                int espaco = (n - 1) / 2;
+                int intx = 1;
+                int plado = ((n - 1) / 2) + 1;
+                int slado = ((n - 1) / 2);
+
+                for (int i = n; i > plado; i--)
+                {
+                    for (int a = 0; a < espaco; a++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int a = 0; a < intx; a++)
+                    {
+                        Console.Write("x");
+                    }
+                    for (int a = 0; a < espaco; a++)
+                    {
+                        Console.Write(" ");
+                    }
+                    espaco--;
+                    intx += 2;
+                    Console.Write("\n");
+                }
+                for (int i = n; i > slado; i--)
+                {
+                    for (int a = 0; a < espaco; a++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int a = 0; a < intx; a++)
+                    {
+                        Console.Write("x");
+                    }
+                    for (int a = 0; a < espaco; a++)
+                    {
+                        Console.Write(" ");
+                    }
+                    espaco++;
+                    intx -= 2;
+                    Console.Write("\n");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
